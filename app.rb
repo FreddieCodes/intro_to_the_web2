4565cdce7dd1e6f2d857cd367239e34e03957cc2
Frederick Ofori <FrederickCodes@gmail.com>
2017-08-15 14:52:17 +0100

    post ed params

--- app.rb
+++ app.rb
@@ -12,12 +12,35 @@ get "/secret/lol" do
   "LMAOOOO"
 end
 
+require "sinatra"
+
+get "/" do
+  "hello!"
+end
+
+get "/secret" do
+  "I was joking"
+end
+
+get "/secret/lol" do
+  "LMAOOOO"
+end
+
+get "/code" do
+  "CODE IS LIFE"
+end
+
+get "/decision" do
+  @location = ["Heaven", "Not so heaven"].sample
+  erb(:index)
+end
+
 get "/code" do
   "CODE IS LIFE"
 end
 
 get "/random-cat" do
-  @name = ["Jazz", "Fidel Catro", "Colonel Cattafi"].sample
+  @name = ["Jazz", "Fidel Catro", "Colonel Cattafi", "Cat Stevens"].sample
   erb(:index)
 end
 
